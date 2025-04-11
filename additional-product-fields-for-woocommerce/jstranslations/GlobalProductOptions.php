68344e1dae1a85e9a70673946df04dbb12a3783f
Sean Murphy <sean@iamseanmurphy.com>
2025-04-11 01:46:33 +0000

    1.2.144

--- additional-product-fields-for-woocommerce/jstranslations/GlobalProductOptions.php
+++ additional-product-fields-for-woocommerce/jstranslations/GlobalProductOptions.php
@@ -9,11 +9,11 @@
 "Is Empty"=>__("Is Empty","rednaowooextraproduct"),
 "Is Not Empty"=>__("Is Not Empty","rednaowooextraproduct"),
 "Contains"=>__("Contains","rednaowooextraproduct"),
+"Not Contains"=>__("Not Contains","rednaowooextraproduct"),
 "Greater Than"=>__("Greater Than","rednaowooextraproduct"),
 "Greater or Equal Than"=>__("Greater or Equal Than","rednaowooextraproduct"),
 "Less Than"=>__("Less Than","rednaowooextraproduct"),
 "Less or Equal Than"=>__("Less or Equal Than","rednaowooextraproduct"),
-"Not Contains"=>__("Not Contains","rednaowooextraproduct"),
 "Is Checked"=>__("Is Checked","rednaowooextraproduct"),
 "Is Not Checked"=>__("Is Not Checked","rednaowooextraproduct")
 );
