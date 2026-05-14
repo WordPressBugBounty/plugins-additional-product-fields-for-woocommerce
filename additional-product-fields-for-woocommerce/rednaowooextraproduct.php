200b48c9ab781417b55b3e81bc0b813f7664a21c
Sean Murphy <sean@iamseanmurphy.com>
2026-05-14 15:04:56 +0000

    1.2.165

--- additional-product-fields-for-woocommerce/rednaowooextraproduct.php
+++ additional-product-fields-for-woocommerce/rednaowooextraproduct.php
@@ -5,7 +5,7 @@
  * Description: Create extra options to each of your products
  * Author: RedNao
  * Author URI: http://rednao.com
- * Version: 1.2.164
+ * Version: 1.2.165
  * Text Domain: additional-product-fields-for-woocommerce
  * Domain Path: /languages/
  * Network: true
